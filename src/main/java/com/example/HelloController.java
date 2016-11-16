d252f3f75210f34c1c36841a74e497420c85f5a4
Andrew Ripka <aripka@pivotal.io>
2016-11-15 22:47:14 -0600

    add hello with greeting onjsct json

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -1,12 +1,25 @@
 package com.example;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+	private final AtomicInteger counter = new AtomicInteger();
+
 	@RequestMapping("/")
 	public String index() {
 		return "Greetings from Omaha Spring Boot Starter App!";
 	}
+
+	@RequestMapping(value = "/hello-world")
+	public @ResponseBody Greeting sayHello(@RequestParam(value = "name", defaultValue = "Stranger") String name) {
+		return new Greeting(counter.incrementAndGet(), "Hello, " + name);
+	}
+
 }
